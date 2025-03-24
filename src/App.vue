aef4ae619878f7af4749b6e4e5ae70b1cd38e61d
yl <ZHXSI1324@163.com>
2025-03-24 16:08:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    123
+    <components1 />
+  </div>
+</template>
+
+<script setup>
+import components1 from './components/components1.vue'
+</script>
+
+<style lang="scss" scoped></style>
